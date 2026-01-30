af5525886cae394dafe8a6b31242375aa1359991
Saul <twinreacko@gmail.com>
2026-01-29 20:19:10 -0600

    Primeros programas en Java

---
+++ Datos.java
@@ -0,0 +1,10 @@
+public class Datos {
+    public static void main(String[] args) {
+
+        String nombre = "Saul";
+        int edad = 18;
+
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Edad: " + edad);
+    }
+}
